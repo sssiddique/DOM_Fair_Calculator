3763465c3a451511fb20412f597e571d7ad3f875
Syeed Saleh Siddique <67413501+sssiddique@users.noreply.github.com>
2022-03-20 16:06:27 +0600

    With functional calculator
    Copy button is not done yet

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-let vehicleVal = document.getElementById("vehicle");
-let distanceVal = document.getElementById("distance");
-let calc = document.getElementById("calculate");
-console.log(vehicleVal.value);
 
-distanceVal.parseInt("distanceVal");
-
-document.getElementById('btn').addEventListener('click', function(){
-    switch( vehicleVal, distanceVal){
-        case vehicleVal=="bus" && distanceVal=="100":
-            calc.value = distanceVal*10;
-        break;
-        default:
-            console.log("None");
-            break;
+function rent(vehicleVal, distanceVal){
+    if(vehicleVal=='bus' && distanceVal=='10'){
+        return parseInt(distanceVal)*10;
+    }else if(vehicleVal=='bus' && distanceVal=='15'){
+        return parseInt(distanceVal)*10;
+    }else if(vehicleVal=='bus' && distanceVal=='20'){
+        return parseInt(distanceVal)*10;
+    }else if(vehicleVal=='car' && distanceVal=='10'){
+        return parseInt(distanceVal)*15;
+    }else if(vehicleVal=='car' && distanceVal=='15'){
+        return parseInt(distanceVal)*15;
+    }else if(vehicleVal=='car' && distanceVal=='20'){
+        return parseInt(distanceVal)*15;
+    }else if(vehicleVal=='train' && distanceVal=='10'){
+        return parseInt(distanceVal)*20;
+    }else if(vehicleVal=='train' && distanceVal=='15'){
+        return parseInt(distanceVal)*20;
+    }else if(vehicleVal=='train' && distanceVal=='20'){
+        return parseInt(distanceVal)*20;
+    }else{
+        return 0;
     }
+};
+
+
+let vehicleSel = document.getElementById("vehicle");
+let distanceSel = document.getElementById("distance");
+document.getElementById('btn').addEventListener('click', function hello(){
+    
+    x = vehicleSel.value;
+    y = distanceSel.value;
+    // console.log(rent(vehicleSel,distanceSel));
+    let output = rent(x,y);
+    let calc = document.getElementById("calculate");
+    calc.value = output;
 });
